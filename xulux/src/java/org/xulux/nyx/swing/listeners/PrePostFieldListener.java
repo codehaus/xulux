f123b12570138317933e29842def3cc16edad53e
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2003-06-17 12:09:22 +0000

    Moved listeners to a more appropiate package

---
+++ xulux/src/java/org/xulux/nyx/swing/listeners/PrePostFieldListener.java
@@ -0,0 +1,126 @@
+/*
+ $Id: PrePostFieldListener.java,v 1.11 2003-06-17 12:09:22 mvdb Exp $
+
+ Copyright 2002 (C) The Xulux Project. All Rights Reserved.
+ 
+ Redistribution and use of this software and associated documentation
+ ("Software"), with or without modification, are permitted provided
+ that the following conditions are met:
+
+ 1. Redistributions of source code must retain copyright
+    statements and notices.  Redistributions must also contain a
+    copy of this document.
+ 
+ 2. Redistributions in binary form must reproduce the
+    above copyright notice, this list of conditions and the
+    following disclaimer in the documentation and/or other
+    materials provided with the distribution.
+ 
+ 3. The name "xulux" must not be used to endorse or promote
+    products derived from this Software without prior written
+    permission of The Xulux Project.  For written permission,
+    please contact martin@mvdb.net.
+ 
+ 4. Products derived from this Software may not be called "xulux"
+    nor may "xulux" appear in their names without prior written
+    permission of the Xulux Project. "xulux" is a registered
+    trademark of the Xulux Project.
+ 
+ 5. Due credit should be given to the Xulux Project
+    (http://xulux.org/).
+ 
+ THIS SOFTWARE IS PROVIDED BY THE XULUX PROJECT AND CONTRIBUTORS
+ ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ THE XULUX PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+package org.xulux.nyx.swing.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import org.xulux.nyx.gui.NyxListener;
+import org.xulux.nyx.gui.Widget;
+import org.xulux.nyx.gui.swing.Entry;
+
+/**
+ * 
+ * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
+ * @version $Id: PrePostFieldListener.java,v 1.11 2003-06-17 12:09:22 mvdb Exp $
+ */
+public class PrePostFieldListener extends NyxListener
+implements FocusListener, ActionListener
+{
+    
+    /**
+     * Constructor for PrePostFieldListener.
+     */
+    public PrePostFieldListener(Widget widget)
+    {
+        super(widget);
+    }
+
+    /**
+     * now call pre.. 
+     * @see java.awt.event.FocusListener#focusGained(FocusEvent)
+     */
+    public void focusGained(FocusEvent e)
+    {
+        if (isProcessing())
+        {
+            return;
+        }
+        if (e.getID() != FocusEvent.FOCUS_GAINED || e.isTemporary())
+        {
+            return;
+        }
+        started();
+    }
+
+    /**
+     * now call post..
+     * @see java.awt.event.FocusListener#focusLost(FocusEvent)
+     */
+    public void focusLost(FocusEvent e)
+    {
+        if (isProcessing())
+        {
+            return;
+        }
+        if (e.getID() != FocusEvent.FOCUS_LOST || e.isTemporary())
+        {
+            return;
+        }
+        System.out.println("Widget clazz : "+widget.getClass().getName());
+        if (widget instanceof Entry)
+        {
+            System.out.println("Widget value : "+widget.getValue());
+        }
+        completed();
+    }
+
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        if (isProcessing())
+        {
+            return;
+        }
+        completed();
+    }
+    
+
+}
