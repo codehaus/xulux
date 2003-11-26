7f0c865d069561a5edbae748b97314e10f1c5948
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2003-11-26 11:01:16 +0000

    Updated test, no widgetList is completely covered

--- xulux/src/test/org/xulux/nyx/global/DictionaryTest.java
+++ xulux/src/test/org/xulux/nyx/global/DictionaryTest.java
@@ -1,8 +1,8 @@
 /*
- $Id: DictionaryTest.java,v 1.20 2003-11-13 02:45:39 mvdb Exp $
+ $Id: DictionaryTest.java,v 1.21 2003-11-26 11:01:16 mvdb Exp $
 
  Copyright 2002-2003 (C) The Xulux Project. All Rights Reserved.
- 
+
  Redistribution and use of this software and associated documentation
  ("Software"), with or without modification, are permitted provided
  that the following conditions are met:
@@ -10,29 +10,29 @@
  1. Redistributions of source code must retain copyright
     statements and notices.  Redistributions must also contain a
     copy of this document.
- 
+
  2. Redistributions in binary form must reproduce the
     above copyright notice, this list of conditions and the
     following disclaimer in the documentation and/or other
     materials provided with the distribution.
- 
+
  3. The name "xulux" must not be used to endorse or promote
     products derived from this Software without prior written
-    permission of The Xulux Project.  For written permission,
+    permission of The Xulux Project. For written permission,
     please contact martin@mvdb.net.
- 
+
  4. Products derived from this Software may not be called "xulux"
     nor may "xulux" appear in their names without prior written
     permission of the Xulux Project. "xulux" is a registered
     trademark of the Xulux Project.
- 
+
  5. Due credit should be given to the Xulux Project
     (http://xulux.org/).
- 
+
  THIS SOFTWARE IS PROVIDED BY THE XULUX PROJECT AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
  THE XULUX PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
@@ -41,7 +41,7 @@
  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 package org.xulux.nyx.global;
 
@@ -57,36 +57,35 @@ import junit.framework.TestSuite;
 
 /**
  * Tests the initialization of the dictionary.
- * Note: the warnings you get during the test are anticipated, it is just to test 
- * how nyx handles bogus entry in the dictionary xml. 
- * 
+ * Note: the warnings you get during the test are anticipated, it is just to test
+ * how nyx handles bogus entry in the dictionary xml.
+ *
  * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
- * @version $Id: DictionaryTest.java,v 1.20 2003-11-13 02:45:39 mvdb Exp $
+ * @version $Id: DictionaryTest.java,v 1.21 2003-11-26 11:01:16 mvdb Exp $
  */
-public class DictionaryTest extends TestCase
-{
+public class DictionaryTest extends TestCase {
 
     /**
      * Constructor for DictionaryTest.
-     * @param name
+     * @param name the test name
      */
-    public DictionaryTest(String name)
-    {
+    public DictionaryTest(String name) {
         super(name);
     }
 
-    public static Test suite()
-    {
+    /**
+     * @return the testsuite
+     */
+    public static Test suite() {
         TestSuite suite = new TestSuite(DictionaryTest.class);
         return suite;
     }
 
     /**
-     * Tests the initialization of the dictaionary from an dictionary 
+     * Tests the initialization of the dictaionary from an dictionary
      * file
      */
-    public void testInitialize()
-    {
+    public void testInitialize() {
         System.out.println("testInitialize");
         Dictionary dictionary = Dictionary.getInstance();
         dictionary.initialize(this.getClass().getClassLoader().getResourceAsStream("org/xulux/nyx/global/dictionary.xml"));
@@ -99,12 +98,11 @@ public class DictionaryTest extends TestCase
         assertNotNull(list.get(list.indexOf("street")));
         assertNotNull(list.get(list.indexOf("subBean")));
     }
-    
-    /** 
-     * Tests for dynamic mapping 
+
+    /**
+     * Tests for dynamic mapping
      */
-    public void testEasyMapping()
-    {
+    public void testEasyMapping() {
         System.out.println("testEasyMapping");
         Dictionary d = Dictionary.getInstance();
         DictionaryBean bean = new DictionaryBean();
@@ -113,7 +111,7 @@ public class DictionaryTest extends TestCase
         // Testing setting a new value on the bean..
         IField field = mapping.getField("name");
         field.setValue(bean, "name");
-        assertEquals("name",field.getValue(bean));
+        assertEquals("name", field.getValue(bean));
         // test if the widget pointer is actually stripped..
         field = mapping.getField("?Prefix:Table.name");
         assertNotNull(field);
@@ -132,14 +130,13 @@ public class DictionaryTest extends TestCase
         mapping = d.getMapping("Idontexist");
         assertNull(mapping);
     }
-    
+
     /**
      * Tests if everything works ok when overriding
      * of a bean is used.
      * Also test if booleans are working correctly.
      */
-    public void testNestedDataBean()
-    {
+    public void testNestedDataBean() {
         System.out.println("testNestedDataBean");
         Dictionary d = Dictionary.getInstance();
         d.setBaseClass(DictionaryBaseBean.class);
@@ -148,12 +145,15 @@ public class DictionaryTest extends TestCase
         BeanMapping subBean = d.getMapping("DictionarySubSubBean");
         IField field = subBean.getField("nice");
     }
-    
+
+    /**
+     * Test if boolean data is handled correctly
+     */
     public void testBooleanData() {
         System.out.println("testBooleanData");
         Dictionary d = Dictionary.getInstance();
         d.setBaseClass(DictionaryBaseBean.class);
-        BeanMapping subBean = d.getMapping(DictionarySubBean.class,true);
+        BeanMapping subBean = d.getMapping(DictionarySubBean.class, true);
         IField field = subBean.getField("nice");
         assertNotNull(field);
         DictionarySubBean bean = new DictionarySubBean();
@@ -162,12 +162,11 @@ public class DictionaryTest extends TestCase
         bean.setNice(false);
         assertFalse(bean.isNice());
     }
-    
+
     /**
      * Test the Fields/field (non autodiscovery) mechanisme
      */
-    public void testFieldElements()
-    {
+    public void testFieldElements() {
         System.out.println("testFieldElements");
         Dictionary d = Dictionary.getInstance();
         d.initialize(this.getClass().getClassLoader().getResourceAsStream("org/xulux/nyx/global/dictionary.xml"));
@@ -183,20 +182,25 @@ public class DictionaryTest extends TestCase
         assertEquals("plaats", mapping.getField("plaats").getAlias());
         assertEquals("city", mapping.getField("plaats").getName());
     }
-    
-    public void testInfiniteLoop()
-    {
+
+    /**
+     * Test to see if infinite looping is preventing
+     */
+    public void testInfiniteLoop() {
         System.out.println("testInfiniteLoop");
         Dictionary d = Dictionary.getInstance();
         BeanMapping mb = d.getMapping(AnotherRecursiveBean.class);
         // cache should be cleared by dictionary.
         assertTrue(!d.isInCache(AnotherRecursiveBean.class));
         assertTrue(!d.isInCache(RecursiveBean.class));
-        assertEquals(6,mb.getFields().size());
+        assertEquals(6, mb.getFields().size());
         BeanMapping mbmain = d.getMapping(RecursiveBean.class);
         assertEquals(3, mbmain.getFields().size());
     }
-    
+
+    /**
+     * Test parameter processing
+     */
     public void testParameters() {
         System.out.println("testParameters");
         Dictionary d = Dictionary.getInstance();
@@ -206,13 +210,13 @@ public class DictionaryTest extends TestCase
         assertEquals("pb", mapping.getName());
         // Testing setting a new value on the bean..
         assertNull(bean.getParameter("BOGUS"));
-        assertEquals("parameter",mapping.getField("tp").getName());
-        assertEquals("sp",mapping.getField("sp").getAlias());
-        assertEquals("fp",mapping.getField("fp").getAlias());
+        assertEquals("parameter", mapping.getField("tp").getName());
+        assertEquals("sp", mapping.getField("sp").getAlias());
+        assertEquals("fp", mapping.getField("fp").getAlias());
     }
-    
+
     /**
-     * Test if setter like (String, String) 
+     * Test if setter like (String, String)
      * work under nyx.
      * Basic assumption is that if there is a
      * getXXX(String), there will be a setXX(String,String)
@@ -232,17 +236,16 @@ public class DictionaryTest extends TestCase
         assertEquals(bean.getDouble(ParameteredBean.NO1), fieldno1.getValue(bean));
         assertEquals(bean.getDouble(ParameteredBean.NO2), fieldno2.getValue(bean));
         assertEquals(bean.getDouble(ParameteredBean.NO3), fieldno3.getValue(bean));
-        fieldno1.setValue(bean,"NO1NewValue");
-        assertEquals("NO1NewValue",fieldno1.getValue(bean));
-        fieldno2.setValue(bean,"NO2NewValue");
-        assertEquals("NO2NewValue",fieldno2.getValue(bean));
-        fieldno3.setValue(bean,"NO3NewValue");
-        assertEquals("NO3NewValue",fieldno3.getValue(bean));
+        fieldno1.setValue(bean, "NO1NewValue");
+        assertEquals("NO1NewValue", fieldno1.getValue(bean));
+        fieldno2.setValue(bean, "NO2NewValue");
+        assertEquals("NO2NewValue", fieldno2.getValue(bean));
+        fieldno3.setValue(bean, "NO3NewValue");
+        assertEquals("NO3NewValue", fieldno3.getValue(bean));
     }
-    
-    
+
     /**
-     * Test the setting of the setmethod in the 
+     * Test the setting of the setmethod in the
      * dictionary.
      */
     public void testSetMethod() {
@@ -258,7 +261,7 @@ public class DictionaryTest extends TestCase
         assertEquals(bean.getDouble(ParameteredBean.NO2), fieldno2.getValue(bean));
         assertEquals(bean.getDouble(ParameteredBean.NO3), fieldno3.getValue(bean));
     }
-    
+
     /**
      * Test the converter functionality in the registry.
      */
@@ -267,12 +270,12 @@ public class DictionaryTest extends TestCase
         Dictionary.addConverter(IntegerConverter.class);
         Integer i = new Integer(10);
         IConverter converter = Dictionary.getConverter(i);
-        assertEquals(Integer.class,converter.getType());
+        assertEquals(Integer.class, converter.getType());
         assertEquals("10", converter.getGuiValue(i));
         assertEquals(i, converter.getBeanValue("10"));
         assertNull(Dictionary.getConverter(DictionaryTest.class));
     }
-    
+
     /**
      * Test the converter by means of parsing the dictionary.xml
      *
@@ -282,13 +285,13 @@ public class DictionaryTest extends TestCase
         Dictionary dictionary = Dictionary.getInstance();
         dictionary.initialize(this.getClass().getClassLoader().getResourceAsStream("org/xulux/nyx/global/dictionary.xml"));
         Map converters = Dictionary.getConverters();
-        assertEquals(2,converters.size());
-        assertEquals(IntegerConverter.class,Dictionary.getConverter(Integer.class).getClass());
-        assertEquals(DoubleConverter.class,Dictionary.getConverter(Double.class).getClass());
+        assertEquals(2, converters.size());
+        assertEquals(IntegerConverter.class, Dictionary.getConverter(Integer.class).getClass());
+        assertEquals(DoubleConverter.class, Dictionary.getConverter(Double.class).getClass());
     }
-    
+
     /**
-     * If the bean (in this case a list) contains the method get(int) or any 
+     * If the bean (in this case a list) contains the method get(int) or any
      * other get, it would crash.
      */
     public void testGetMappingWithGetMethod() {
@@ -298,26 +301,55 @@ public class DictionaryTest extends TestCase
         assertNotNull(mapping.getField("get"));
         mapping = Dictionary.getInstance().getMapping(new InnerBean());
         assertNotNull(mapping.getField("x"));
-    }        
-        
+    }
+
+    /**
+     * The inner class FieldList is covered here.
+     */
+    public void testFieldList() {
+        System.out.println("testFieldList");
+        BeanMapping.FieldList list = new BeanMapping().new FieldList();
+        list.add("string1");
+        list.add("string2");
+        list.add("string3");
+        assertEquals(3, list.size());
+        assertEquals(-1, list.indexOf(null));
+        assertEquals(0, list.indexOf("string1"));
+        assertEquals(2, list.indexOf("string3"));
+        assertEquals(-1, list.indexOf("string4"));
+        assertEquals(-1, list.indexOf(new Object()));
+    }
+
     /**
      * @see junit.framework.TestCase#setUp()
      */
     protected void setUp() throws Exception {
         Dictionary.reset();
     }
-    
+
+    /**
+     * The innerbean
+     */
     public class InnerBean {
-        
+
+        /**
+         * Constructor
+         */
         public InnerBean() {
         }
-        
+
+        /**
+         * @return X
+         */
         public String getX() {
             return "X";
         }
-        
+
+        /**
+         * @param x x
+         */
         public void setX(String x) {
-        } 
+        }
     }
 
 }
