27a130a2a715aa380845ebe3547d850f874211b9
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2003-11-27 01:09:20 +0000

    Reformat

--- xulux/src/java/org/xulux/nyx/swing/models/DefaultComboModel.java
+++ xulux/src/java/org/xulux/nyx/swing/models/DefaultComboModel.java
@@ -1,5 +1,5 @@
 /*
- $Id: DefaultComboModel.java,v 1.27 2003-11-06 19:53:11 mvdb Exp $
+ $Id: DefaultComboModel.java,v 1.28 2003-11-27 01:09:20 mvdb Exp $
 
  Copyright 2002-2003 (C) The Xulux Project. All Rights Reserved.
 
@@ -18,7 +18,7 @@
 
  3. The name "xulux" must not be used to endorse or promote
     products derived from this Software without prior written
-    permission of The Xulux Project.  For written permission,
+    permission of The Xulux Project. For written permission,
     please contact martin@mvdb.net.
 
  4. Products derived from this Software may not be called "xulux"
@@ -32,7 +32,7 @@
  THIS SOFTWARE IS PROVIDED BY THE XULUX PROJECT AND CONTRIBUTORS
  ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
  NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
  THE XULUX PROJECT OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
@@ -61,30 +61,54 @@ import org.xulux.nyx.swing.widgets.Combo;
  * The default combobox model.
  *
  * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
- * @version $Id: DefaultComboModel.java,v 1.27 2003-11-06 19:53:11 mvdb Exp $
+ * @version $Id: DefaultComboModel.java,v 1.28 2003-11-27 01:09:20 mvdb Exp $
  */
-public class DefaultComboModel extends AbstractListModel
-implements ComboBoxModel
-{
+public class DefaultComboModel extends AbstractListModel implements ComboBoxModel {
+    /**
+     * The original list
+     */
     private List original;
+    /**
+     * the used list
+     */
     private ArrayList list;
+    /**
+     * The selected item
+     */
     private ComboShowable selectedItem;
+    /**
+     * the field to use
+     */
     private String field;
+    /**
+     * the beanmapping
+     */
     private BeanMapping mapping;
+    /**
+     * the combo where this model is used for
+     */
     private Combo combo;
+    /**
+     * If the model has been initialized
+     */
     private boolean initialized;
 
-
-    public DefaultComboModel()
-    {
-        // don't do anything, since we don't have any data..
+    /**
+     * don't do anything, since we don't have any data..
+     * It will create an empty list however
+     */
+    public DefaultComboModel() {
         this.list = new ArrayList();
     }
+
     /**
      * Constructor for DefaultComboModel.
+     *
+     * @param list the list with data
+     * @param field the field to use
+     * @param combo the combo used
      */
-    public DefaultComboModel(List list, String field, Combo combo)
-    {
+    public DefaultComboModel(List list, String field, Combo combo) {
         this.field = field;
         this.original = list;
         this.combo = combo;
@@ -102,25 +126,23 @@ implements ComboBoxModel
      *
      * @see javax.swing.ComboBoxModel#setSelectedItem(Object)
      */
-    public void setSelectedItem(Object anItem)
-    {
-        if (anItem instanceof ComboShowable)
-        {
-            this.selectedItem = (ComboShowable)anItem;
-            if (initialized && !combo.isRefreshing())
-            {
+    public void setSelectedItem(Object anItem) {
+        if (anItem instanceof ComboShowable) {
+            this.selectedItem = (ComboShowable) anItem;
+            if (initialized && !combo.isRefreshing()) {
                 combo.setLazyValue(getRealSelectedValue());
             }
-        }
-        else
-        {
+        } else {
             setRealSelectedValue(anItem);
         }
-        fireContentsChanged(this,-1,-1);
+        fireContentsChanged(this, -1, -1);
     }
 
-    public void setSelectedItem(int index)
-    {
+    /**
+     * Set the selected item to the specified index
+     * @param index the index to select
+     */
+    public void setSelectedItem(int index) {
         if (list.size() > 0) {
             this.selectedItem = (ComboShowable) list.get(index);
         }
@@ -129,18 +151,15 @@ implements ComboBoxModel
     /**
      * @see javax.swing.ComboBoxModel#getSelectedItem()
      */
-    public Object getSelectedItem()
-    {
+    public Object getSelectedItem() {
         return selectedItem;
     }
 
     /**
      * @see javax.swing.ListModel#getSize()
      */
-    public int getSize()
-    {
-        if (list == null)
-        {
+    public int getSize() {
+        if (list == null) {
             return 0;
         }
         return list.size();
@@ -149,116 +168,100 @@ implements ComboBoxModel
     /**
      * @see javax.swing.ListModel#getElementAt(int)
      */
-    public Object getElementAt(int index)
-    {
-        return list.get(index);
-    }
-
-
-    public Object getRealSelectedValue()
-    {
-        Object retValue = null;
-        if (original != null && selectedItem != null)
-        {
-            retValue =  original.get(selectedItem.getIndex());
-        }
-        else if (selectedItem == null && list != null &&
-                  list.size() > 0)
-        {
-            retValue =  list.get(0);
-        }
-        return retValue;
-    }
-
-    public Object getComboObject(int index)
-    {
+    public Object getElementAt(int index) {
         return list.get(index);
     }
 
     /**
-     * Returns the selected index in the model
-     * If the selection is null, it will return -1
+     * @return the real selected value in the combo, so not a comboshowable
      */
-    public int getSelectedIndex()
-    {
-        if (selectedItem == null)
-        {
-            return -1;
+    public Object getRealSelectedValue() {
+        Object retValue = null;
+        if (original != null && selectedItem != null) {
+            retValue = original.get(selectedItem.getIndex());
+        } else if (selectedItem == null && list != null && list.size() > 0) {
+            retValue = list.get(0);
         }
-        else
-        {
+        return retValue;
+    }
+
+    /**
+     * @param index the index to get the object from
+     * @return the object that is present in the combo
+     *          Normally this will be a comboshowable
+     */
+    public Object getComboObject(int index) {
+        return list.get(index);
+    }
+
+    /**
+     * @return the selected index in the model
+     *          If the selection is null, it will return -1
+     */
+    public int getSelectedIndex() {
+        if (selectedItem == null) {
+            return -1;
+        } else {
             return selectedItem.getIndex();
         }
     }
 
-
-
-    public void setRealSelectedValue(Object selectedItem)
-    {
+    /**
+     * Set the real selected item (so not the comboshowable.
+     *
+     * @param selectedItem the real selected item
+     */
+    public void setRealSelectedValue(Object selectedItem) {
         if (original == null) {
             return;
         }
         int index = original.indexOf(selectedItem);
-//        System.err.println("index : "+index);
-//        System.err.println("selectedItem : "+selectedItem.getClass());
-//        System.err.println("selectedItem : "+selectedItem);
-//        System.err.println("original : "+original);
-        if (index != -1)
-        {
+        //        System.err.println("index : "+index);
+        //        System.err.println("selectedItem : "+selectedItem.getClass());
+        //        System.err.println("selectedItem : "+selectedItem);
+        //        System.err.println("original : "+original);
+        if (index != -1) {
             setSelectedItem(list.get(index));
-        }
-        else
-        {
+        } else {
             setSelectedItem(0);
             combo.setLazyValue(getRealSelectedValue());
         }
     }
 
-    private void initialize()
-    {
-        if (field != null && original.size() > 0)
-        {
+    /**
+     * Initialize the model and the data
+     */
+    private void initialize() {
+        if (field != null && original.size() > 0) {
             int startMappingAt = 0;
-            if (original.get(0).equals(combo.getNotSelectedValue()))
-            {
+            if (original.get(0).equals(combo.getNotSelectedValue())) {
                 startMappingAt = 1;
             }
-            if ((startMappingAt == 1 && original.size() > 1)
-                || startMappingAt == 0)
-            {
+            if ((startMappingAt == 1 && original.size() > 1) || startMappingAt == 0) {
                 Class tmpClazz = original.get(startMappingAt).getClass();
                 mapping = Dictionary.getInstance().getMapping(tmpClazz);
             }
         }
         list = new ArrayList();
-        for (int i=0; i < original.size(); i++)
-        {
+        for (int i = 0; i < original.size(); i++) {
             Object object = original.get(i);
-            if (object != null && (i == 0 && object.equals(combo.getNotSelectedValue()))
-               || this.field == null)
-            {
+            if (object != null && (i == 0 && object.equals(combo.getNotSelectedValue())) || this.field == null) {
                 list.add(new ComboShowable(i, object.toString()));
-            }
-            else
-            {
+            } else {
                 String value = "";
                 StringTokenizer stn = new StringTokenizer(field, ",");
-                    while (stn.hasMoreTokens())
-                    {
-                        String token = stn.nextToken();
-                        String strPart = "";
-                        IField iField = mapping.getField(token);
-                        if (iField != null)
-                        {
-                            strPart = iField.getValue(object).toString();
-                        }
-                        else
-                        {
-                            //allow seperators in the field list.
-                            strPart = token;
-                        }
-                        value+=strPart;
+                while (stn.hasMoreTokens()) {
+                    String token = stn.nextToken();
+                    String strPart = "";
+                    IField iField = mapping.getField(token);
+                    if (iField != null) {
+                        strPart = iField.getValue(object).toString();
+                    } else {
+                        //allow seperators in the field list.
+                        strPart = token;
                     }
+                    value += strPart;
+                }
                 list.add(new ComboShowable(i, value));
             }
         }
@@ -268,37 +271,49 @@ implements ComboBoxModel
     /**
      * Holds all items in a list
      */
-    public class ComboShowable
-    {
+    public class ComboShowable {
+        /**
+         * the index
+         */
         private int index;
+        /**
+         * the value
+         */
         private String value;
 
-        public ComboShowable(int index, String value)
-        {
+        /**
+         * The main constructor
+         * @param index the index in the original data
+         * @param value the value that needs to be shown
+         */
+        public ComboShowable(int index, String value) {
             this.index = index;
             this.value = value;
         }
 
-        public String toString()
-        {
+        /**
+         * Since a combo is using the toString value of an object,
+         * this will return the value set in the constructor
+         *
+         * @see java.lang.Object#toString()
+         */
+        public String toString() {
             return this.value;
         }
 
-        public int getIndex()
-        {
+        /**
+         * @return the index in the original data
+         */
+        public int getIndex() {
             return index;
         }
     }
     /**
      * Clean up combomodel (if at all needed)
      */
-    public void destroy()
-    {
+    public void destroy() {
         this.combo = null;
-        if (this.list != null)
-        {
-            this.list = null;
-        }
+        this.list = null;
         this.mapping = null;
         this.original = null;
         this.field = null;
