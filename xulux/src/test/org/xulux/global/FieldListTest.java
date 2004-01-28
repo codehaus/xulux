24ad11bd1b2b4ec5e8a0070dde44e3447146edf0
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2004-01-28 15:22:09 +0000

    Switching license to apache license 2.

--- xulux/src/test/org/xulux/global/FieldListTest.java
+++ xulux/src/test/org/xulux/global/FieldListTest.java
@@ -1,3 +1,20 @@
+/*
+   $Id: FieldListTest.java,v 1.2 2004-01-28 15:22:07 mvdb Exp $
+   
+   Copyright 2002-2004 The Xulux Project
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package org.xulux.global;
 
 import junit.framework.Test;
@@ -7,7 +24,7 @@ import junit.framework.TestSuite;
 /**
  * 
  * @author Martin van den Bemt
- * @version $Id: FieldListTest.java,v 1.1 2003-12-23 01:20:31 mvdb Exp $
+ * @version $Id: FieldListTest.java,v 1.2 2004-01-28 15:22:07 mvdb Exp $
  */
 public class FieldListTest extends TestCase {
 
