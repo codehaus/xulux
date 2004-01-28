02e99b85d40e12b5188550079204772c448a463c
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2004-01-28 15:02:20 +0000

    Switching license to apache license 2.

--- xulux/src/java/org/xulux/gui/events/SetValueWithoutRefreshEvent.java
+++ xulux/src/java/org/xulux/gui/events/SetValueWithoutRefreshEvent.java
@@ -1,3 +1,20 @@
+/*
+   $Id: SetValueWithoutRefreshEvent.java,v 1.2 2004-01-28 15:00:22 mvdb Exp $
+   
+   Copyright 2002-2004 The Xulux Project
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 package org.xulux.gui.events;
 
 import org.xulux.gui.Widget;
@@ -6,7 +23,7 @@ import org.xulux.gui.XuluxEvent;
 /**
  *
  * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
- * @version $Id: SetValueWithoutRefreshEvent.java,v 1.1 2004-01-28 12:24:00 mvdb Exp $
+ * @version $Id: SetValueWithoutRefreshEvent.java,v 1.2 2004-01-28 15:00:22 mvdb Exp $
  */
 public class SetValueWithoutRefreshEvent implements XuluxEvent {
 
