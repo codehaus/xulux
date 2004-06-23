47a06763db4f273e4c0a9be3216fa0da929289b9
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2004-06-23 11:10:39 +0000

    Optmized some code...

--- xulux/src/java/org/xulux/swing/models/NyxTableModel.java
+++ xulux/src/java/org/xulux/swing/models/NyxTableModel.java
@@ -1,5 +1,5 @@
 /*
-   $Id: NyxTableModel.java,v 1.6 2004-04-01 16:15:09 mvdb Exp $
+   $Id: NyxTableModel.java,v 1.7 2004-06-23 11:10:39 mvdb Exp $
    
    Copyright 2002-2004 The Xulux Project
 
@@ -22,6 +22,7 @@ import java.util.List;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
 
+import org.xulux.core.XuluxContext;
 import org.xulux.dataprovider.Dictionary;
 import org.xulux.dataprovider.IField;
 import org.xulux.dataprovider.IMapping;
@@ -29,13 +30,14 @@ import org.xulux.dataprovider.converters.IConverter;
 import org.xulux.gui.NyxListener;
 import org.xulux.gui.Widget;
 import org.xulux.swing.widgets.Table;
+import org.xulux.utils.BooleanUtils;
 
 /**
  * The nyx tablemodel contains all magic for tables.
  * @todo Assumes lists right now.. Should support more probably..
  *
  * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
- * @version $Id: NyxTableModel.java,v 1.6 2004-04-01 16:15:09 mvdb Exp $
+ * @version $Id: NyxTableModel.java,v 1.7 2004-06-23 11:10:39 mvdb Exp $
  */
 public class NyxTableModel extends NyxListener implements TableModel {
 
@@ -128,9 +130,9 @@ public class NyxTableModel extends NyxListener implements TableModel {
      */
     public String getColumnName(int columnIndex) {
         if (hasModel()) {
-            return "" + model.getColumnName(columnIndex);
+            return model.getColumnName(columnIndex);
         }
-        return "" + columnIndex;
+        return String.valueOf(columnIndex);
     }
 
     /**
@@ -151,8 +153,7 @@ public class NyxTableModel extends NyxListener implements TableModel {
             return model.isCellEditable(rowIndex, columnIndex);
         }
         String editable = ((Widget) table.getChildWidgets().get(columnIndex)).getProperty("editable");
-
-        return "true".equalsIgnoreCase(editable);
+        return BooleanUtils.toBoolean(editable);
     }
 
     /**
@@ -169,12 +170,13 @@ public class NyxTableModel extends NyxListener implements TableModel {
         }
         Widget w = (Widget) table.getChildWidgets().get(columnIndex);
         if (w.getField() != null) {
-            IMapping map = Dictionary.getInstance().getMapping(((List) table.getContent()).get(rowIndex));
+            Object obj = ((List) table.getContent()).get(rowIndex);
+            IMapping map = XuluxContext.getDictionary().getMapping(obj);
             IField field = map.getField(w.getField());
             if (field != null) {
-                Object value = field.getValue(((List) table.getContent()).get(rowIndex));
+                Object value = field.getValue(obj);
                 if (value == null) {
-                    value = "";
+                   return "";
                 }
                 IConverter converter = Dictionary.getConverter(value);
                 if (converter != null) {
@@ -198,7 +200,7 @@ public class NyxTableModel extends NyxListener implements TableModel {
         }
         Widget w = (Widget) table.getChildWidgets().get(columnIndex);
         if (w.getField() != null) {
-            IMapping map = Dictionary.getInstance().getMapping(((List) table.getContent()).get(rowIndex));
+            IMapping map = XuluxContext.getDictionary().getMapping(((List) table.getContent()).get(rowIndex));
             IField field = map.getField(w.getField());
             if (field != null) {
                 field.setValue(((List) table.getContent()).get(rowIndex), aValue);
