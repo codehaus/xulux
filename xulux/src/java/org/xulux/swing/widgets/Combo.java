1ac910d856cc1d366d2c494a1ba509888ca476a7
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2004-10-20 17:26:40 +0000

    Now setting content changed on init

--- xulux/src/java/org/xulux/swing/widgets/Combo.java
+++ xulux/src/java/org/xulux/swing/widgets/Combo.java
@@ -1,5 +1,5 @@
 /*
-   $Id: Combo.java,v 1.6 2004-07-07 17:43:42 mvdb Exp $
+   $Id: Combo.java,v 1.7 2004-10-20 17:26:40 mvdb Exp $
    
    Copyright 2002-2004 The Xulux Project
 
@@ -35,7 +35,7 @@ import org.xulux.swing.util.NyxEventQueue;
  * The swing combo widget.
  *
  * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
- * @version $Id: Combo.java,v 1.6 2004-07-07 17:43:42 mvdb Exp $
+ * @version $Id: Combo.java,v 1.7 2004-10-20 17:26:40 mvdb Exp $
  */
 public class Combo extends NyxCombo {
     /**
@@ -114,6 +114,7 @@ public class Combo extends NyxCombo {
         if (this.initialized) {
             return;
         }
+        contentChanged = true;
         this.initialized = true;
         String nsv = getProperty("notselectedvalue");
         if (nsv != null) {
