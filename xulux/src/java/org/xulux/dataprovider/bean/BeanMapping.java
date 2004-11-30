c488fc4f8e461aff92b85f1558f63b91eb556f3b
mvdb <mvdb@cee8c64b-ab72-0410-a008-e88fd0df2434>
2004-11-30 17:30:54 +0000

    Now searching untill we have found a method with zero parameters. It will return the first method found when no methods without a paremater are present.

--- xulux/src/java/org/xulux/dataprovider/bean/BeanMapping.java
+++ xulux/src/java/org/xulux/dataprovider/bean/BeanMapping.java
@@ -1,5 +1,5 @@
 /*
-   $Id: BeanMapping.java,v 1.3 2004-10-20 17:26:58 mvdb Exp $
+   $Id: BeanMapping.java,v 1.4 2004-11-30 17:30:54 mvdb Exp $
    
    Copyright 2002-2004 The Xulux Project
 
@@ -38,7 +38,7 @@ import org.xulux.dataprovider.IMapping;
  * @todo Also fix the set when realField is used.
  *
  * @author <a href="mailto:martin@mvdb.net">Martin van den Bemt</a>
- * @version $Id: BeanMapping.java,v 1.3 2004-10-20 17:26:58 mvdb Exp $
+ * @version $Id: BeanMapping.java,v 1.4 2004-11-30 17:30:54 mvdb Exp $
  */
 public class BeanMapping implements IMapping
 {
@@ -187,16 +187,24 @@ public class BeanMapping implements IMapping
         } else {
             pre = "get";
         }
-        for (int i = 0; i < methods.length; i++)
-        {
+        Method result = null;
+        for (int i = 0; i < methods.length; i++) {
             Method method = methods[i];
-            if (method.getName().equalsIgnoreCase(pre + name)) {
-                return method;
-            } else if (!setMethod && method.getName().equalsIgnoreCase("is" + name)) {
-                return method;
+            if (method.getName().equalsIgnoreCase(pre + name)  ||
+                !setMethod && method.getName().equalsIgnoreCase("is" + name)) {
+                if (result != null) {
+                  if (result.getParameterTypes().length != 0) {
+                    continue;
+                  }
+                  result = method;
+                  if (result.getParameterTypes().length == 0) {
+                      // we found the correct one..
+                      break;
+                  }
+                }
             }
         }
-        return null;
+        return result;
     }
 
     /**
@@ -370,6 +378,9 @@ public class BeanMapping implements IMapping
                         fieldName = field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
                     }
                     Method setMethod = findMethod(fieldName, true);
+                    String mName = setMethod.getName();
+                    // sanity check of having found the correct method..
+                    
                     field.setChangeMethod(setMethod);
                     addField(field);
                 }
